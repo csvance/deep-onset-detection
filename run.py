be2dd84be136ecfbbc47ba80f2449b82a77fa3ea
Carroll Vance <cs.vance@icloud.com>
2020-12-07 22:17:27 -0600

    Fix class weights / loss shape

--- run.py
+++ run.py
@@ -1,35 +1,47 @@
-from pytorch_lightning import LightningModule, Trainer
 from torch.utils.data import Dataset, DataLoader
 from torch.optim.lr_scheduler import OneCycleLR
-from torch_optimizer import Lookahead
-from sklearn.metrics import roc_auc_score
+from torch_optimizer import Lookahead, SGDW
+from sklearn.metrics import roc_auc_score, confusion_matrix
+from sklearn.utils.class_weight import compute_class_weight
 import pytorch_lightning as pl
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning import LightningModule, Trainer
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import plac
 import numpy as np
 
-D = 16
+D = 32
 BN_EPS = 1e-5
 BN_MOM = 0.99
 BATCH_SIZE = 16
 WEIGHT_DECAY = 1e-2
-EPOCHS = 30
-LR_MAX = 0.01
+EPOCHS = 70
+LR_MAX = 0.001
 
 CFG = [
-    {'repeat': 2, 'dim': int(1 * D), 'expand': 4, 'stride': 2, 'final': False},
-    {'repeat': 3, 'dim': int(2 * D), 'expand': 4, 'stride': 2, 'final': False},
-    {'repeat': 1, 'dim': int(4 * D), 'expand': 4, 'stride': 2, 'final': True},
+    {'repeat': 2, 'dim': int(1 * D), 'expand': 1, 'stride': 2, 'final': False},
+    {'repeat': 3, 'dim': int(2 * D), 'expand': 1, 'stride': 2, 'final': False},
+    {'repeat': 1, 'dim': int(4 * D), 'expand': 1, 'stride': 2, 'final': True},
 ]
 
 
 class OnsetDataset(Dataset):
-    def __init__(self, X, y):
+    def __init__(self, X, y, training: bool = True):
         self.X = X
-        self.y = y
-        self.accuracy = pl.metrics.Accuracy()
+        self.y = y.astype(np.int64)
+
+        self.training = training
+
+        if len(self.y.shape) == 2:
+            self.w = compute_class_weight(y=[1 if len(np.where(yi)[0]) > 0 else 0 for yi in self.y],
+                                          class_weight="balanced",
+                                          classes=[0, 1])
+        else:
+            self.w = compute_class_weight(y=self.y,
+                                          class_weight="balanced",
+                                          classes=[0, 1])
 
     def __len__(self):
         return len(self.X)
@@ -44,21 +56,25 @@ class OnsetDataset(Dataset):
             y = 1 if y_pos > 0 else 0
 
             if y_pos == 0:
-                w = 1.
+                w = self.w[0]
             else:
                 y_pos_pct = y_pos / (y_pos + y_neg)
+                y_neg_pct = y_neg / (y_pos + y_neg)
 
                 if y_pos_pct < 0.5:
-                    w = min(2., 4 * y_pos / (y_pos + y_neg))
+                    w = self.w[1]*2*min(1., 2 * y_pos_pct)
                 else:
-                    w = min(2., 4 * y_neg / (y_pos + y_neg))
+                    w = self.w[1]*2*min(1., 2 * y_neg_pct)
         else:
-            w = 1.
             y = self.y[item]
+            w = self.w[[0, 1].index(y)]
+
+        # Don't discount loss if not training
+        if not self.training:
+            w = self.w[[0, 1].index(y)]
 
         X = X.transpose((1, 0)).astype(np.float32)
-        w = np.array([w]).astype(np.float32)[0]
-        y = int(y)
+        w = np.array([w]).astype(np.float32)
         return X, y, w
 
 
@@ -123,7 +139,7 @@ class OnsetModule(LightningModule):
             if not cfg['final']:
                 block = ResnetBlock(c_in=c_in, c_out=cfg['dim'], expand=cfg['expand'], stride=cfg['stride'])
             else:
-                features = cfg['dim'] * cfg['expand']
+                features = 4 * cfg['dim']
                 block = ResnetBlock(c_in=c_in, c_out=features, expand=cfg['expand'], stride=cfg['stride'])
 
             self.blocks.append(block)
@@ -137,7 +153,6 @@ class OnsetModule(LightningModule):
         self.bn_head = nn.BatchNorm1d(features, eps=BN_EPS, momentum=1 - BN_MOM)
         self.relu_head = nn.ReLU()
         self.pool_head = nn.AdaptiveMaxPool1d((1,))
-        self.dropout = nn.Dropout(0.3)
         self.fc = nn.Linear(features, 2)
 
     def forward(self, x):
@@ -159,16 +174,15 @@ class OnsetModule(LightningModule):
         x = self.bn_head(x)
         x = self.relu_head(x)
         x = self.pool_head(x)[:, :, 0]
-        x = self.dropout(x)
-        x = self.fc(x)
+        y = self.fc(x)
 
-        return x
+        return y
 
     def training_step(self, batch, batch_nb):
-        X, y_target, alpha = batch
+        X, y_target, w = batch
 
         y = self.forward(X)
-        loss = torch.mean(alpha*F.cross_entropy(y, y_target, reduction="none"))
+        loss = torch.mean(w * torch.unsqueeze(F.cross_entropy(y, y_target, reduction="none"), dim=-1))
 
         self.log('train_loss', loss, prog_bar=False, logger=True)
         self.log('lr', self.optimizers().param_groups[0]['lr'])
@@ -180,9 +194,9 @@ class OnsetModule(LightningModule):
         X, y_target, w = batch
         y = self(X)
 
-        loss = F.cross_entropy(y, y_target)
+        loss = torch.mean(w * torch.unsqueeze(F.cross_entropy(y, y_target, reduction="none"), dim=-1))
 
-        self._test_pred.append(F.softmax(y.detach())[:, 1].cpu().numpy())
+        self._test_pred.append(F.softmax(y.detach(), dim=-1)[:, 1].cpu().numpy())
         self._test_true.append(y_target.detach().cpu().numpy())
 
         return {'val_loss': loss}
@@ -197,18 +211,24 @@ class OnsetModule(LightningModule):
 
         self.log('val_auc', roc_auc_score(self._test_true, self._test_pred))
 
+        tn, fp, fn, tp = confusion_matrix(y_true=self._test_true.astype(np.int),
+                                          y_pred=np.round(self._test_pred).astype(np.int)).ravel()
+
+        print("\n\n----Confusion Matrix----")
+        print("\t[neg\t\tpos\t\t]")
+        print("neg\t[%d\t\t%d\t]" % (tn, fp))
+        print("pos\t[%d\t\t%d\t]\n" % (fn, tp))
+
         self._test_true = []
         self._test_pred = []
-
-        return {'val_loss': avg_loss}
 
     def test_step(self, batch, batch_nb):
         X, y_target, w = batch
         y = self(X)
 
-        loss = F.cross_entropy(y, y_target)
+        loss = torch.mean(w * torch.unsqueeze(F.cross_entropy(y, y_target, reduction="none"), dim=-1))
 
-        self._test_pred.append(F.softmax(y.detach())[:, 1].cpu().numpy())
+        self._test_pred.append(F.softmax(y.detach(), dim=-1)[:, 1].cpu().numpy())
         self._test_true.append(y_target.detach().cpu().numpy())
 
         return {'test_loss': loss}
@@ -223,18 +243,24 @@ class OnsetModule(LightningModule):
 
         self.log('test_auc', roc_auc_score(self._test_true, self._test_pred))
 
-        self._test_true = []
-        self._test_pred = []
+        tn, fp, fn, tp = confusion_matrix(y_true=self._test_true.astype(np.int),
+                                          y_pred=np.round(self._test_pred).astype(np.int)).ravel()
+
+        print("\n\n----Confusion Matrix----")
+        print("\t[neg\t\tpos\t\t]")
+        print("neg\t[%d\t\t%d\t]" % (tn, fp))
+        print("pos\t[%d\t\t%d\t]\n" % (fn, tp))
 
         self._test_true = []
         self._test_pred = []
 
-        return {'test_loss': avg_loss}
+        self._test_true = []
+        self._test_pred = []
 
     def configure_optimizers(self):
-        inner_optimizer = torch.optim.SGD(self.parameters(),
-                                          lr=0.0001,
-                                          weight_decay=WEIGHT_DECAY)
+        inner_optimizer = SGDW(self.parameters(),
+                               lr=0.0001,
+                               weight_decay=WEIGHT_DECAY)
         optimizer = Lookahead(inner_optimizer)
         schedule = {'scheduler': OneCycleLR(inner_optimizer,
                                             max_lr=LR_MAX,
@@ -254,7 +280,7 @@ class OnsetModule(LightningModule):
                           batch_size=BATCH_SIZE)
 
     def val_dataloader(self):
-        return DataLoader(OnsetDataset(self.X_test, self.y_test),
+        return DataLoader(OnsetDataset(self.X_test, self.y_test, training=False),
                           shuffle=True,
                           batch_size=BATCH_SIZE)
 
@@ -274,6 +300,7 @@ def main():
 
     model = OnsetModule((X_train, y_train), (X_test, y_test))
 
+    logger = TensorBoardLogger('lightning_logs')
     trainer = Trainer(gpus=1,
                       precision=32,
                       max_epochs=EPOCHS,
